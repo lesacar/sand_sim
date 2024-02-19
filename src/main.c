d5d64afcd3f170bed9e5786f59be531650c2e970
lesacar <balkangamescroatia@gmail.com>
2024-02-19 20:58:36 +0100

    .

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "raylib.h"
+#include <raylib.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
@@ -18,16 +18,14 @@ int main(int argc, char** argv)
     int current_monitor = handle_arguments(argc, argv); if (current_monitor < 0) { exit(EXIT_FAILURE); } 
     set_monitor_and_fps(current_monitor); 
 
-    float cur_dt = 0.0f;
-    float last_dt = cur_dt;
+    float cur_dt = 0.0f; float last_dt = cur_dt;
     
     while (!WindowShouldClose()) {
-        last_dt = cur_dt;
-        cur_dt = GetFrameTime();
+        last_dt = cur_dt; cur_dt = GetFrameTime();
         BeginDrawing();
-        ClearBackground(WHITE);
+        ClearBackground(BLACK);
         
-        DrawText(TextFormat("%.2f", (double)((float)1.0/(float)cur_dt)), 20, 20, 20, BLACK);
+        DrawText(TextFormat("%.2f", (double)((float)1.0/(float)cur_dt)), 20, 20, 20, WHITE);
         EndDrawing(); 
     }
     return 0;

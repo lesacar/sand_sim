604819da40e4fbe4270bab6fa36d6af83b3dcf5e
lesacar <balkangamescroatia@gmail.com>
2024-02-20 03:17:56 +0100

    .

--- sand_blocks.glsl
+++ sand_blocks.glsl
@@ -1,15 +1,22 @@
+// sand_blocks.glsl
 #version 330 core
 
 in vec2 fragCoord;
 out vec4 fragColor;
 
-uniform int grid[1440][720]; // Assuming maximum dimensions
+uniform sampler2D gridTexture; // Texture containing the grid data
 
 void main()
 {
-    int block = grid[int(fragCoord.x)][int(fragCoord.y)];
-    if (block > 0)
+    vec4 block = texture(gridTexture, fragCoord);
+    
+    // Render rectangles only if the current cell contains material
+    if (block.r > 0.0)
+    {
         fragColor = vec4(201.0 / 255.0, 170.0 / 255.0, 127.0 / 255.0, 1.0); // Sand block color
+    }
     else
-        discard; // Don't render empty cells
+    {
+        discard; // Discard fragments for empty cells
+    }
 }
